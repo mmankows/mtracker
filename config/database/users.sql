462675cf3e6b03212f0379b03c815de18e93208d
majname <dum@pad.com>
2015-05-05 20:12:30 +0200

    More config

---
+++ config/database/users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users` (
+    `user_id`       bigint(5) NOT NULL auto_increment,
+    `device_id`     char(30) default NULL,
+    `tracked_from` timestamp NOT NULL default CURRENT_TIMESTAMP,
+    PRIMARY KEY  (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1001 DEFAULT CHARSET=latin2;
