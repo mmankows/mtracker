a13f3ff13dce796560a568e4ca0b31cfdcb2ba4d
majname <dum@pad.com>
2015-06-01 22:13:31 +0200

    Super

--- mtracker/config/database/tracked_routes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `tracked_routes` (
-  `step_id`     bigint(10) NOT NULL auto_increment,
-  `user_id`     bigint(5) NOT NULL,
-  `latitude`    decimal(10,7) NOT NULL,
-  `longitude`   decimal(10,7) NOT NULL,
-  `logdate`     timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`step_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=187 DEFAULT CHARSET=latin2; 
